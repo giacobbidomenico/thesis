2249d64bac5d63fe41975654f9c65b592a97c74d
domenico.giacobbi <domenico.giacobbi@studio.unibo.it>
2024-02-06 22:22:09 +0100

    fix: fix button-led bugs

--- esp32/src/Configuration.h
+++ esp32/src/Configuration.h
@@ -5,7 +5,7 @@
 #define PASSWORD_WIFI "mioEsp1833"
 
 #define DEVICE_GROUP 2
-#define MQTT_SERVER "192.168.32.154"
+#define MQTT_SERVER "192.168.200.154"
 #define PORT 1883
 
 #define TOPIC_INPUT "thesisIngSInfUnibo2024Input"
